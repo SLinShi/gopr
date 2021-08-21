2439f496dfe181ba017369108b772b585b07f64b
shilin <shilin.it@qq.com>
2021-08-21 20:07:34 +0800

    update something

--- ch1/os.go
+++ ch1/os.go
@@ -1,13 +1,15 @@
 package main
-import(
+
+import (
 	"fmt"
 	"os"
 )
+
 func main() {
-	var s,sep string
-	for i := 1;i<len(os.Args);i++{
-		s+=sep + os.Args[i]
-		sep=" "
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
 	}
 	fmt.Println(s)
 }
